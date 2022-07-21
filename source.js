23c76ed0208a0e9eee07016ed2661dbe35961efa
dasx10 <dasx10@gmail.com>
2022-07-21 19:20:52 +0300

    fix length

--- source.js
+++ source.js
@@ -4,12 +4,20 @@ const curryLimit = Object.assign(function (limit, executor, ...parameters) {
     switch (arguments.length) {
         case 0  : return curryLimit;
         case 1  : return (executor, ...parameters) => curryLimit(limit, executor, ...parameters);
-        default : return limit < parameters.length
-            ? (...nextParameters) => curryLimitCore(limit, ...parameters, ...nextParameters)
+        default : {
+            let parametersLength = parameters.length;
+            return limit < parametersLength
+            ? Object.defineProperty((...nextParameters) => curryLimitCore(limit, executor, ...parameters, ...nextParameters), 'length', {
+                enumerable   : false,
+                configurable : false,
+                writable     : false,
+                value        : limit - parametersLength
+            })
             : executor(...parameters);
+        }
     };
 }, {
-    core: require('fup-curry-limit-core')
+    core: curryLimitCore
 });
 
 module.export = curryLimit;
